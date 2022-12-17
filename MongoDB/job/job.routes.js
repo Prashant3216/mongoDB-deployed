c74f15dc147b32532162ff4be8c6fdd2abc3a5ce
Prashant3216 <pv3216@gmail.com>
2022-12-17 14:40:47 +0530

    complete all the api

--- MongoDB/job/job.routes.js
+++ MongoDB/job/job.routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const jobModel=require("./job.model")
 
 const app = Router();
+
+// Post Data API
 app.post("/", async (req, res) => {
     let body=req.body
     // console.log(body)
@@ -13,10 +15,41 @@ app.post("/", async (req, res) => {
     }
   });
 
-
+// get Data API
   app.get("/joblist", async (req, res) => {
     try {
       let products=await jobModel.find();
+      return res.status(200).send(products);
+    } catch (error) {
+      return res.status(404).send("Something went wrong");
+    }
+  });
+
+//   Filter API with Role
+  app.get("/joblist/filter", async (req, res) => {
+    let role=req.query
+    try {
+      let products=await jobModel.find({role:role});
+      return res.status(200).send(products);
+    } catch (error) {
+      return res.status(404).send("Something went wrong");
+    }
+  });
+
+// Sort API from latest_to_oldest
+  app.get("/joblist/sort/latest_to_oldest", async (req, res) => {
+    try {
+      let products=await jobModel.find().sort({postedAt:-1});
+      return res.status(200).send(products);
+    } catch (error) {
+      return res.status(404).send("Something went wrong");
+    }
+  });
+// Search API with query
+  app.get("/joblist/search", async (req, res) => {
+    let lenguage=req.query
+    try {
+      let products=await jobModel.find({lenguage:lenguage});
       return res.status(200).send(products);
     } catch (error) {
       return res.status(404).send("Something went wrong");
