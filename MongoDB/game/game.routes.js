0810ce00a428bd5c32787e0449600b4dda1c0c5b
Prashant3216 <pv3216@gmail.com>
2022-12-20 13:10:59 +0530

    added random word api

--- MongoDB/game/game.routes.js
+++ MongoDB/game/game.routes.js
@@ -21,4 +21,25 @@ app.post("/", async (req, res) => {
       return res.status(404).send("Something went wrong");
     }
   });
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+
+  app.get("/random/word", async (req, res) => {
+    try {
+    let alpha="abcdefghijklmnopqrstuvwxyz"
+    let n=getRandomIntInclusive(1,26)
+    let str=""
+    for(let i=0; i<n; i++){
+       str+=alpha[getRandomIntInclusive(0,25)]
+    }
+    return res.status(200).send({"string":str});
+      
+    } catch (error) {
+      return res.status(404).send("Something went wrong");
+    }
+  });
   module.exports = app;
