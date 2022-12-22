cb33f997d12e2efe59c76a72762178bec4308dcc
Prashant3216 <pv3216@gmail.com>
2022-12-22 13:55:07 +0530

    added api

--- MongoDB/question/question.routes.js
+++ MongoDB/question/question.routes.js
@@ -5,14 +5,12 @@ const app = Router();
 
 // get questions by filter  API
 app.get("/", async (req, res) => {
-    let { category, difficulty, ques } = req.query;
-    ques= +ques
+  let { category, difficulty, ques } = req.query;
+  ques = +ques;
+  console.log(category, difficulty, ques);
   // console.log(body)
   try {
-    let questions= await qesModel.aggregate([
-        { $match: {$and:[{ category: category}, {difficulty:difficulty}]}},
-        { $sample: { size: ques } },
-      ]);
+    let questions = await qesModel.find({category:category, difficulty:difficulty}).limit(ques)
     return res.status(200).send(questions);
   } catch (error) {
     return res.status(405).send(error.message);
