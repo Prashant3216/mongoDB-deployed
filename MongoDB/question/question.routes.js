b77d8a262ad498e81537a8acb0bf2211e25681c0
Prashant3216 <pv3216@gmail.com>
2022-12-22 12:45:29 +0530

    corrected code

--- MongoDB/question/question.routes.js
+++ MongoDB/question/question.routes.js
@@ -3,56 +3,18 @@ const qesModel = require("./question.model");
 
 const app = Router();
 
-// Post Data API
-app.post("/", async (req, res) => {
-  let body = req.body;
+// get questions by filter  API
+app.get("/", async (req, res) => {
+    let { category, difficulty, ques } = req.query;
   // console.log(body)
   try {
-    await qesModel.insertMany(body);
-    return res.status(200).send("Job added successfully");
+    let questions= await qesModel.aggregate([
+        { $match: { category: category, difficulty: difficulty } },
+        { $sample: { size: ques } },
+      ]);
+    return res.status(200).send(questions);
   } catch (error) {
     return res.status(405).send(error.message);
-  }
-});
-
-// get Data API
-app.get("/joblist", async (req, res) => {
-  try {
-    let products = await qesModel.find();
-    return res.status(200).send(products);
-  } catch (error) {
-    return res.status(404).send("Something went wrong");
-  }
-});
-
-//   Filter API with Role
-app.get("/joblist/filter", async (req, res) => {
-  let { role } = req.query;
-  try {
-    let products = await qesModel.find({ role: role });
-    return res.status(200).send(products);
-  } catch (error) {
-    return res.status(404).send("Something went wrong");
-  }
-});
-
-// Sort API from latest_to_oldest
-app.get("/joblist/sort/latest_to_oldest", async (req, res) => {
-  try {
-    let products = await qesModel.find().sort({ postedAt: -1 });
-    return res.status(200).send(products);
-  } catch (error) {
-    return res.status(404).send("Something went wrong");
-  }
-});
-// Search API with query
-app.get("/joblist/search", async (req, res) => {
-  let { language } = req.query;
-  try {
-    let products = await qesModel.find({ language: language });
-    return res.status(200).send(products);
-  } catch (error) {
-    return res.status(404).send("Something went wrong");
   }
 });
 
--- MongoDB/shopping/shop.routes.js
+++ MongoDB/shopping/shop.routes.js
@@ -3,36 +3,32 @@ const shopModel = require("./shop.model");
 
 const app = Router();
 
-// app.post("/", async (req, res) => {
-//     let body=req.body
-//     // console.log(body)
-//     try {
-//      await shopModel.insertMany({...body,date_timestamp:new Date()});
-//       return res.status(200).send("Product added successfully");
-//     } catch (error) {
-//       return res.status(405).send(error.message);
-//     }
-//   });
+app.post("/", async (req, res) => {
+    let body=req.body
+    // console.log(body)
+    try {
+     await shopModel.insertMany({...body,date_timestamp:new Date()});
+      return res.status(200).send("Product added successfully");
+    } catch (error) {
+      return res.status(405).send(error.message);
+    }
+  });
 app.get("/", async (req, res) => {
-  let { category, difficulty, ques } = req.query;
   try {
-    let questions = await shopModel.aggregate([
-      { $match: { category: category, difficulty: difficulty } },
-      { $sample: { size: ques } },
-    ]);
-    return res.status(200).send(questions);
+    let products = await shopModel.find();
+    return res.status(200).send(products);
   } catch (error) {
     return res.status(404).send("Something went wrong");
   }
 });
-// app.delete("/products/:id", async (req, res) => {
-//   let {id}=req.params
-//   try {
-//     await shopModel.deleteOne({"_id":id});
-//     return res.status(200).send("deleted");
-//   } catch (error) {
-//     return res.status(404).send("Something went wrong");
-//   }
-// });
+app.delete("/products/:id", async (req, res) => {
+  let {id}=req.params
+  try {
+    await shopModel.deleteOne({"_id":id});
+    return res.status(200).send("deleted");
+  } catch (error) {
+    return res.status(404).send("Something went wrong");
+  }
+});
 
 module.exports = app;
